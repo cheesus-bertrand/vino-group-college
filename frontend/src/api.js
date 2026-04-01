0697d29de958b398287693a1cf9a19a22e730c75
Jessica <jessica.stpierregagne@gmail.com>
2026-04-01 14:03:00 -0400

    updater le vite.config et api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://127.0.0.1:8000",
+  baseURL: "/api",
+  withCredentials: true,
 });
+
+export const fetchCsrfToken = () => api.get("/csrf-token");
 
 export default api;
