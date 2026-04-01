a3b6570279094ae17c5e46ccc68dac48847bd004
Jessica <jessica.stpierregagne@gmail.com>
2026-04-01 13:47:28 -0400

    Ajouter les routes dans api.php pour l'authentification

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('web')->group(function () {
+    Route::post('/login', [AuthController::class, 'store'])->name('login');
+    Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
+
+    Route::get('/csrf-token', function () {
+        return response()->json(['csrf_token' => csrf_token()]);
+    });
+});
+
+
