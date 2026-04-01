0de9a2c7e73a5eb474ec6fd27b1823de7bf05241
imane-new-me <i.elmostaqim.ca@gmail.com>
2026-04-01 17:29:04 -0400

    ST-73 nettoyer code

--- backend/app/Http/Controllers/VinController.php
+++ backend/app/Http/Controllers/VinController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Vin;
 use App\Services\SAQService;
-use Symfony\Component\VarDumper\VarDumper;
 
 class VinController extends Controller
 {
